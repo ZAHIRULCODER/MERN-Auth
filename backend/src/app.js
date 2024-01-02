1fcd432c041647c4a603d7839586f74f4be5d305
SK ZAHIRUL ISLAM <skzahirulislam22@gmail.com>
2024-01-02 21:15:08 +0530

    Add route for testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,10 @@ app.use(express.json({ limit: "30kb" }));
 app.use(express.urlencoded({ extended: true, limit: "30kb" }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+	res.send("Hello from express. For testing");
+});
+
 //routes declaration
 app.use("/api/v1/users", userRouter);
 
