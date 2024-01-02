d1a350632670fac82ca176b29a388f4011931f29
SK ZAHIRUL ISLAM <skzahirulislam22@gmail.com>
2024-01-02 23:48:38 +0530

    Update API endpoints to use production URL

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,14 +1,19 @@
 //Auth
-export const REGISTER_USER = "http://localhost:3000/api/v1/users/register";
-export const LOGIN_USER = "http://localhost:3000/api/v1/users/login";
-export const LOGOUT_USER = "http://localhost:3000/api/v1/users/logout";
+export const REGISTER_USER =
+	"https://mern-auth-backend-zeta.vercel.app/api/v1/users/register";
+export const LOGIN_USER =
+	"https://mern-auth-backend-zeta.vercel.app/api/v1/users/login";
+export const LOGOUT_USER =
+	"https://mern-auth-backend-zeta.vercel.app/api/v1/users/logout";
 
 //Users
-export const CURRENT_USER = "http://localhost:3000/api/v1/users/current-user";
+export const CURRENT_USER =
+	"https://mern-auth-backend-zeta.vercel.app/api/v1/users/current-user";
 export const CHANGE_PASSWORD =
-	"http://localhost:3000/api/v1/users/change-password";
+	"https://mern-auth-backend-zeta.vercel.app/api/v1/users/change-password";
 export const UPDATE_ACCOUNT_DETAILS =
-	"http://localhost:3000/api/v1/users/update-account-details";
+	"https://mern-auth-backend-zeta.vercel.app/api/v1/users/update-account-details";
 
 // Token
-export const REFRESH_TOKEN = "http://localhost:3000/api/v1/users/refresh-token";
+export const REFRESH_TOKEN =
+	"https://mern-auth-backend-zeta.vercel.app/api/v1/users/refresh-token";
