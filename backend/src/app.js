1db54675ce18b5ceb926982f0e419b6fbbd4c54f
SK ZAHIRUL ISLAM <skzahirulislam22@gmail.com>
2024-01-02 20:32:32 +0530

    Add environment variables and remove commented code

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json({ limit: "30kb" }));
 app.use(express.urlencoded({ extended: true, limit: "30kb" }));
-// app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes declaration
